afce7420e2185a26caca7592d9eaec22ad688ca2
jakobus97 <jakobus97@gmail.com>
2025-05-08 22:02:52 +1000

    Add files via upload

---
+++ site-quiz-data.js
@@ -0,0 +1,8 @@
+
+const siteData = [
+  { "Site": "ALLORA - GRAINX", "Site State": "QLD", "Latitude": -28.039564, "Longitude": 151.981975 },
+  { "Site": "Apamurra - VIT", "Site State": "SA", "Latitude": -34.856906, "Longitude": 139.196535 },
+  { "Site": "ARDLETHAN - GNC", "Site State": "NSW", "Latitude": -34.354915, "Longitude": 146.88881 },
+  { "Site": "ARDLETHAN - LDC", "Site State": "NSW", "Latitude": -34.356033, "Longitude": 146.891984 },
+  { "Site": "ARDROSSAN - VIT", "Site State": "SA", "Latitude": -34.436705, "Longitude": 137.905761 }
+];
